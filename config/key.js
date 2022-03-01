1f2a04400823988fb927d60f595f346ebdc17f33
anandstephan <34913953+anandstephan@users.noreply.github.com>
2022-03-01 22:23:19 +0530

    change in AWS key

--- config/key.js
+++ config/key.js
@@ -1,7 +1,8 @@
 module.exports = {
   MongoURI:
-    "mongodb+srv://anand:1234567890@cluster0.xm9io.mongodb.net/myFirstDatabase?retryWrites=true&w=majority",
-  accessKeyId: "AKIA3IUQOLXKWOXWSRFE",
-  secretAccessKey: "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",
+    "mongodb+srv://scrollmantra:scrollmantra123@cluster0.84ow4.mongodb.net/myFirstDatabase?retryWrites=true&w=majority",
+    accessKeyId: "AKIA3IUQOLXKUSRKRDSQ",
+    secretAccessKey: "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",
   region: "ap-south-1",
+  correctClockSkew: true
 };
